d4132a66549fb6763a817d52b0f649aa505455a1
Pavel Senin <seninp@gmail.com>
2020-03-25 07:49:52 +0100

    producing a growth plot

--- data_ingestor_coronadatascraper.R
+++ data_ingestor_coronadatascraper.R
@@ -1,25 +1,86 @@
 library(readr)
 library(plyr)
 library(dplyr)
+library(reshape2)
+
 library(ggplot2)
 library(viridisLite)
+library(gridExtra)
+library(Cairo)
 
 # https://coronadatascraper.com/#sources
 # https://github.com/lazd/coronadatascraper
-
 dd = read_csv("https://coronadatascraper.com/timeseries-tidy.csv")
 
-dd_fr = filter(dd, country=="FRA", population=67106161, grepl("opencovid19-fr", url))
-unique(dd_fr$type)
-dd_cases = plyr::ddply(dd_fr, .(date), summarize, max_cases = max(value, na.rm=T))
-
-
-dd_cases = as.data.frame(select(filter(dd_fr, type=="cases"), date, value, type, url))
-unique(dd_cases$url)
-
-p <- ggplot(dd_cases, aes(x=date, y=max_cases)) +
-  geom_line( color="steelblue") + 
+# FRANCE
+dd_fr = filter(dd, country=="FRA", grepl("opencovid19-fr", url))
+dd_fr = filter(dd_fr, population==max(dd_fr$population, na.rm=T))
+dd_fr_cases = as.data.frame(select(filter(dd_fr, type=="cases"), date, value))
+p_growth <- ggplot(dd_fr_cases, aes(x=date, y=value)) +
+  geom_line() + 
   geom_point() +
   xlab("") +
-  theme(axis.text.x=element_text(angle=60, hjust=1))
-p
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  ggtitle("FRANCE: Cov19 cases growth")
+p_growth
+
+# ITALY
+dd_it = filter(dd, country=="ITA")
+dd_it = filter(dd_it, population==max(dd_it$population, na.rm=T))
+dd_it_cases = as.data.frame(select(filter(dd_it, type=="cases"), date, value))
+p_growth <- ggplot(dd_it_cases, aes(x=date, y=value)) +
+  geom_line() + 
+  geom_point() +
+  xlab("") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  ggtitle("GERMANY: Cov19 cases growth")
+p_growth
+
+# SPAIN
+dd_sp = filter(dd, country=="ESP")
+dd_sp = filter(dd_sp, population==max(dd_sp$population, na.rm=T))
+dd_sp_cases = as.data.frame(select(filter(dd_sp, type=="cases"), date, value))
+p_growth <- ggplot(dd_sp_cases, aes(x=date, y=value)) +
+  geom_line() + 
+  geom_point() +
+  xlab("") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  ggtitle("SPAIN: Cov19 cases growth")
+p_growth
+
+# GERMANY
+dd_gr = filter(dd, country=="DEU")
+dd_gr = filter(dd_gr, population==max(dd_gr$population, na.rm=T))
+dd_gr_cases = as.data.frame(select(filter(dd_gr, type=="cases"), date, value))
+p_growth <- ggplot(dd_gr_cases, aes(x=date, y=value)) +
+  geom_line() + 
+  geom_point() +
+  xlab("") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  ggtitle("GERMANY: Cov19 cases growth")
+p_growth
+
+# MERGE
+dd_m <- data.frame(date=seq(as.Date("01/01/2020", "%d/%m/%y"), Sys.Date()-1, by='day'))
+dd_m <- Reduce(function(...) merge(..., by='date', all=TRUE),
+             list(dd_m, dd_fr_cases, dd_it_cases, dd_sp_cases, dd_gr_cases))
+names(dd_m) <- c("date", "France", "Italy", "Germany", "Spain")
+dd_m <- filter(dd_m, date>as.Date("15/02/2020", "%d/%m/%y"))
+
+dm <- reshape2::melt(dd_m, id=1)
+p_growth <- ggplot(dm, aes(x=date, y=value, col=variable)) +
+  geom_line(size=.5) + geom_point(size=2) +
+  labs(x="Date, 2020", y="Confirmed cases") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  ggtitle("Cov19 cases growth") +
+  scale_colour_viridis_d(name="Countries") + theme_dark() +
+  theme(panel.background=element_rect(fill = "lightgrey", colour = "grey", size = 0.5, linetype = "solid"),
+    panel.grid.major=element_line(size = 0.5, linetype = 'solid',colour = "white"), 
+    panel.grid.minor = element_line(size = 0.25, linetype = 'solid',colour = "white"),
+    legend.background = element_rect(fill="lightgrey", size=0.5, linetype="solid", colour ="grey"),
+    legend.key = element_rect(fill="grey75", size=0.5, linetype="solid", colour ="grey60"),
+    legend.key.width = unit(20, unit = "pt")
+  )
+p_growth
+
+ggsave(p_growth, filename = "figures/total_growth.png", width = 8, height = 6, dpi = 400, type = "cairo")
--- data_ingestor_sante_publique.R
+++ data_ingestor_sante_publique.R
@@ -6,14 +6,16 @@ library(viridisLite)
 
 ######################
 dd = read_csv("https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv")
-
 dd_fr = filter(dd, maille_code=="FRA", maille_nom=="France", source_type=="sante-publique-france")
-
-str(dd_fr)
-
-p <- ggplot(dd_fr, aes(x=date, y=cas_confirmes)) +
-  geom_line( color="steelblue") + 
+p_growth <- ggplot(dd_fr, aes(x=date, y=cas_confirmes)) +
+  geom_line() + 
   geom_point() +
   xlab("") +
-  theme(axis.text.x=element_text(angle=60, hjust=1))
-p
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  ggtitle("fRANCE: Cov19 cases growth")
+p_growth
+
+# https://timchurches.github.io/blog/posts/2020-02-18-analysing-covid-19-2019-ncov-outbreak-data-with-r-part-1/#estimating-changes-in-the-effective-reproduction-number
+
+library(earlyR)
+?get_R
